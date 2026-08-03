12024e2aa7940fb9f339b52f3168d906f9b9b18d
purwadikasuryatkjsmeksa25-cmd <purwadikasuryatkjsmeksa25@gmail.com>
2026-08-03 14:46:27 +0700

    Add files via upload

---
+++ ganjil genap.py
@@ -0,0 +1,6 @@
+angka = int(input("Masukkan bilangan: "))
+
+if angka % 2 == 0:
+      print("Bilangan Genap")
+else:
+    print("Bilangan Ganjil")
---
+++ luas persegi pnjang.py
@@ -0,0 +1,5 @@
+p = int(input("masukan panjaang persegi panjang (cm) : "))
+L = int(input("masukan lebar persegi panjang (cm) : "))
+
+luas = p * L
+print("Luas persegi panjang :", luas, "cm")
