d85b3ce792497cd12382035baa55edc67da94f58
purwadikasuryatkjsmeksa25-cmd <purwadikasuryatkjsmeksa25@gmail.com>
2026-08-24 14:05:02 +0700

    perulangan

--- ganjilgenapp.py
+++ ganjilgenapp.py
@@ -1,6 +1,12 @@
-angka = int(input("Masukkan angka: "))
+while True:
+    angka = int(input("masukan bilangan baginda: "))
 
-if angka % 2 == 0:
-    print("Bilangan Genap")
-else:
-    print("Bilangan Ganjil")
+    if angka % 2 == 0:
+        print("Bilangan Genap")
+    else:
+        print("Bilangan Ganjil")
+
+    keluar = input("Mau keluar baginda? (y/n): ")
+
+    if keluar == "y":
+        break
