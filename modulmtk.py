7fc086b10541aa528c0baacb6e97bf656c60b380
purwadikasuryatkjsmeksa25-cmd <purwadikasuryatkjsmeksa25@gmail.com>
2026-08-31 14:10:24 +0700

    mengandi ganjil genap dan prima

--- modulmtk.py
+++ modulmtk.py
@@ -4,36 +4,18 @@ def ganjil_genap(angka):
     else:
         return "Bilangan Ganjil"
 
-
 def bilangan_prima(angka):
     if angka < 2:
         return False
 
-    for i in range(2, angka):
+    pembagi = 0
+
+    for i in range(1, angka + 1):
         if angka % i == 0:
-            return False
+            pembagi += 1
 
-    return True
-
-def luas_persegi(sisi):
-    return sisi * sisi
-
-
-def luas_persegi_panjang(panjang, lebar):
-    return panjang * lebar
-
-
-def luas_segitiga(alas, tinggi):
-    return 0.5 * alas * tinggi
-
-
-def luas_lingkaran(jari_jari):
-    return 3.14 * jari_jari * jari_jari
-
-
-def keliling_persegi(sisi):
-    return 4 * sisi
-
-
-def keliling_persegi_panjang(panjang, lebar):
+    if pembagi == 2:
+        return True
+    else:
+        return False
     return 2 * (panjang + lebar)
