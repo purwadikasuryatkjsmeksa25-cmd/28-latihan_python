db3001f91b9320029e41ebf26e9ff5d1b3c1a05c
purwadikasuryatkjsmeksa25-cmd <purwadikasuryatkjsmeksa25@gmail.com>
2026-08-31 12:50:16 +0700

    menambahkan funtion dan menu

--- ganjilgenapp.py
+++ ganjilgenapp.py
@@ -1,12 +1,46 @@
-while True:
-    angka = int(input("masukan bilangan baginda: "))
-
+def cek_ganjil_genap(angka):
     if angka % 2 == 0:
-        print("Bilangan Genap")
+        return "Bilangan Genap"
     else:
-        print("Bilangan Ganjil")
+        return "Bilangan Ganjil"
 
-    keluar = input("Mau keluar baginda? (y/n): ")
 
-    if keluar == "y":
+# Function Bilangan Prima
+def cek_prima(angka):
+    if angka < 2:
+        return False
+
+    for i in range(2, angka):
+        if angka % i == 0:
+            return False
+
+    return True
+
+
+# Menu
+while True:
+    print("\n=== MENU ===")
+    print("1. Cek Ganjil/Genap")
+    print("2. Cek Bilangan Prima")
+    print("3. Keluar")
+
+    pilihan = input("Pilih menu: ")
+
+    if pilihan == "1":
+        angka = int(input("Masukkan angka: "))
+        print(cek_ganjil_genap(angka))
+
+    elif pilihan == "2":
+        angka = int(input("Masukkan angka: "))
+
+        if cek_prima(angka):
+            print("Bilangan Prima")
+        else:
+            print("Bukan Bilangan Prima")
+
+    elif pilihan == "3":
+        print("Program selesai")
         break
+
+    else:
+        print("Pilihan tidak tersedia")
